1753636680ebd68469a4b4469ff81113e52c1ec1
gerald manalac <it.geraldomanalac@gmail.com>
2021-02-02 12:54:27 +0800

    Zettabyte

---
+++ zettabyte.php
@@ -0,0 +1,10 @@
+<?php
+ echo "Git 101!";
+echo "Zettabyte";
+echo "Members:";
+echo "Gerald Mañalac";
+echo "Kerwin Agasa";
+echo "John Carlo Pineda";
+echo "Daryll Lu";
+echo "Julius Castro Jose";
+?>
